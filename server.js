b63c5e71214e1331aa74fb4bc92a7b4985500b4e
Kalungez <muthomititus+github@gmail.com>
2020-05-05 17:27:29 +0300

    work on error handling by using mongoose middleware

--- server.js
+++ server.js
@@ -1,7 +1,9 @@
 const express = require("express");
 const dotenv = require("dotenv");
-// const logger = require('./middleware/logger')
-const morgan = require('morgan')
+const morgan = require("morgan");
+const colors = require('colors')
+const errorHandler = require('./middleware/error')
+const connectDB = require("./config/db");
 
 // routes
 const bootcamps = require("./router/bootcamps");
@@ -9,19 +11,31 @@ const bootcamps = require("./router/bootcamps");
 // load env file
 dotenv.config({ path: "./config/config.env" });
 
+// Connect to db
+connectDB();
+
 const app = express();
 
-if (process.env.NODE_ENV === 'development'){
-	app.use(morgan('dev'))
+// body parser
+app.use(express.json())
+
+if (process.env.NODE_ENV === "development") {
+	app.use(morgan("dev"));
 }
 
 // mount router
 app.use("/api/v1/bootcamps", bootcamps);
 
+app.use(errorHandler)
 
 PORT = process.env.PORT;
 
-app.listen(
+const server = app.listen(
 	PORT,
-	console.log(`Server Running on poort ${PORT}, in ${process.env.NODE_ENV}`)
+	console.log(`Server Running on poort ${PORT}, in ${process.env.NODE_ENV}`.yellow.bold)
 );
+
+process.on("unhandledRejection", (err, Promise) => {
+	console.log(`Error: ${err.message}`.red);
+	server.close(() => process.exit(1));
+});
