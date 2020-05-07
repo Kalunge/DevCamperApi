6e051d42f50b12cc568b76932b1431d5399ad15b
Kalungez <muthomititus+github@gmail.com>
2020-05-07 12:14:30 +0300

    advanced querying

--- router/bootcamps.js
+++ router/bootcamps.js
@@ -5,11 +5,12 @@ const {
 	updateBootcamp,
 	createBootcamp,
 	deleteBootcamp,
+	getBootcampsInRadius,
 } = require("../controllers/bootcamp");
 const router = express.Router();
 
+router.route("/radius/:zipcode/:distance").get(getBootcampsInRadius);
 router.route("/").get(getBootcamps).post(createBootcamp);
-
 router
 	.route("/:id")
 	.get(getBootcamp)
