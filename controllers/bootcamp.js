7b045f182e5d0d92b975747bf2d3479c32fa70e8
Kalungez <muthomititus+github@gmail.com>
2020-05-07 14:26:04 +0300

    make re-routing to the course router possible

--- controllers/bootcamp.js
+++ controllers/bootcamp.js
@@ -13,11 +13,10 @@ exports.getBootcamps = asyncHandler(async (req, res, next) => {
 	let reqQuery = { ...req.query };
 
 	// exclude fields
-	const removeFields = ["select", "sort"];
+	const removeFields = ["select", "sort", "page", "limit"];
 
 	// loop over remove fields and exclude them from reqQuery
 	removeFields.forEach((param) => delete reqQuery[param]);
-	console.log(reqQuery);
 
 	// create query
 	let queryStr = JSON.stringify(reqQuery);
@@ -45,11 +44,37 @@ exports.getBootcamps = asyncHandler(async (req, res, next) => {
 		query = query.sort("-createdAt");
 	}
 
+	// Pagination
+	const page = parseInt(req.query.page, 10) || 1;
+	const limit = parseInt(req.query.limit, 10) || 25;
+	const startIndex = (page - 1) * limit;
+	const endIndex = page * limit;
+	const	total = await Bootcamp.countDocuments()
+	query = query.skip(startIndex).limit(limit);
+
 	// EXECUTING QUERY
 	const bootcamps = await query;
+
+	// PAGINATION RESULT
+	const pagination = {}
+
+	if (endIndex < total) {
+		pagination.next = {
+			page: page + 1,
+			limit
+		}
+	}
+
+	if (startIndex > 0) {
+		pagination.prev = {
+			page: page - 1,
+			limit
+		}
+	}
+
 	res
 		.status(200)
-		.json({ success: true, count: bootcamps.length, data: bootcamps });
+		.json({ success: true,pagination, count: bootcamps.length, data: bootcamps });
 });
 
 // @desc  Show bootcamp
