7875f68f818388bf0d3be9bd11ec6dc2c2b5c0bb
Kalungez <muthomititus+github@gmail.com>
2020-05-14 18:32:23 +0300

    add functionality to create reviews in the command line

--- seeder.js
+++ seeder.js
@@ -10,6 +10,7 @@ dotenv.config({ path: "./config/config.env" });
 const Bootcamp = require("./models/Bootcamp");
 const Course = require("./models/Course");
 const User = require("./models/User");
+const Review = require("./models/Review");
 
 // Connect to the database
 mongoose.connect(process.env.MONGO_URI, {
@@ -32,12 +33,17 @@ const users = JSON.parse(
 	fs.readFileSync(`${__dirname}/_data/users.json`, "utf-8")
 );
 
+const reviews = JSON.parse(
+	fs.readFileSync(`${__dirname}/_data/reviews.json`, "utf-8")
+);
+
 // import to db
 const importData = async () => {
 	try {
 		await Bootcamp.create(bootcamps);
 		await Course.create(courses);
 		await User.create(users);
+		await Review.create(reviews);
 		console.log("data imported...".green.inverse);
 		process.exit();
 	} catch (err) {
@@ -50,6 +56,7 @@ const deleteData = async () => {
 		await Bootcamp.deleteMany();
 		await Course.deleteMany();
 		await User.deleteMany();
+		await Review.deleteMany();
 		console.log("Data destroyed..".red.inverse);
 		process.exit();
 	} catch (err) {
